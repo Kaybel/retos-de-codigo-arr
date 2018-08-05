7daf3cb5f86ec0b54d64786cd0a5f4d008697f8d
Kaybel <kblmrtnz@gmail.com>
2018-08-05 14:30:20 -0400

    enunciados

---
+++ 11/src/app.js
@@ -0,0 +1,10 @@
+/*
+Escriba una función llamada computeSumOfAllElements.
+
+Dado un array de números, computeSumOfAllElements devuelve la suma de todos los elementos del array dado.
+
+Ejemplo:
+
+var output = computeSumOfAllElements([1, 2, 3]);
+console.log(output); // --> 6
+*/
---
+++ 14/src/app.js
@@ -0,0 +1,11 @@
+/*
+Escribe una función llamada findShortestOfThreeWords.
+
+Dado 3 cadenas, findShortestOfThreeWords devuelve el más corto de las cadenas dadas.
+
+Notas:
+
+Si hay empate, debe devolver la primera palabra en la lista de parámetros.
+var output = findShortestOfThreeWords('a', 'dos', 'tres');
+console.log(salida); // -> 'a'
+*/
